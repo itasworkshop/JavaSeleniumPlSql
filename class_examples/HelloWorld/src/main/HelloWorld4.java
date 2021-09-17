633bc154902e04309508fb27b7c19697f2bbf517
itasworkshop <mepraveens121@gmail.com>
2021-09-17 13:55:39 +0530

    17 sep

--- class_examples/HelloWorld/src/main/HelloWorld4.java
+++ class_examples/HelloWorld/src/main/HelloWorld4.java
@@ -1,7 +1,7 @@
 package main;
 
 
-class Fan1{
+class Fan2{
 
     //String color = "Black";
     //int price = 2500;
@@ -10,13 +10,28 @@ class Fan1{
     String company = "Bajaj";
     String color;
     int price;
+    int numblade;
+
+    Fan2(){
+        this.price = 5000;
+        System.out.println("hello from default constructor.");
+    }
 
     //constructor - looks like a function, with class name, used for initialisation
-    Fan1(int p,String color){
+    //parameterised constructor
+    Fan2(int p,String color){
         System.out.println("hello from constructor.");
         //instance variable -- specific to instances
         this.price = p; //this keyword refers to current objects
         this.color = color;
+    }
+
+    Fan2(int p,String color,int numblade){
+        System.out.println("hello from constructor.");
+        //instance variable -- specific to instances
+        this.price = p; //this keyword refers to current objects
+        this.color = color;
+        this.numblade = numblade;
     }
 
     public void getSwitchedOn(){
@@ -34,15 +49,33 @@ class Fan1{
 }
 
 
-public class HelloWorld3 {
+public class HelloWorld4 {
 
 
 
     public static void main(String[] args){
         System.out.println("Hello World!!!!!!!!");
 
-        Fan1 obj1 = new Fan1(3500,"Brown");
-        Fan1 obj2 = new Fan1(4500,"White");
+        Fan2 obj1 = new Fan2(3500,"Brown");
+        Fan2 obj2 = new Fan2(4500,"White");
+        Fan2 obj3 = new Fan2();
+        Fan2 obj4 = new Fan2(7500,"White",3);
+
+        System.out.println("The price fan 4 is "+ obj4.price);
+        System.out.println("The color fan 4 is "+ obj4.color);
+        System.out.println("The company of fan 4 is "+ obj4.company);
+
+        obj4.getSwitchedOn(); //dot notation
+        obj4.getRotate();
+        obj4.getSwitchedOff();
+
+        System.out.println("The price fan 3 is "+ obj3.price);
+        System.out.println("The color fan 3 is "+ obj3.color);
+        System.out.println("The company of fan 3 is "+ obj3.company);
+
+        obj3.getSwitchedOn(); //dot notation
+        obj3.getRotate();
+        obj3.getSwitchedOff();
 
 
         System.out.println("The price fan 1 is "+ obj1.price);
